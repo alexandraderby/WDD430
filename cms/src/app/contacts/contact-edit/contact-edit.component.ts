bf81a6898a9355e6f2eb467bfd21afe9e4d3578c
Alexandra Derby <alexandraderby@Alexandras-MacBook-Air.local>
2021-11-06 14:30:47 -0700

    contact add and edit complete

--- cms/src/app/contacts/contact-edit/contact-edit.component.ts
+++ cms/src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { Contact } from '../contact.model';
@@ -11,14 +11,38 @@ import { ContactService } from '../contact.service';
   styleUrls: ['./contact-edit.component.css']
 })
 export class ContactEditComponent implements OnInit {
-  groupContacts: [];
+
+  groupContacts: Contact[] = [];
+  originalContact: Contact;
   contact: Contact;
+  editMode: boolean = false;
+  subscription: Subscription;
+  id: string;
 
   constructor(  private contactService: ContactService,
                 private router: Router,
                 private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+
+  ngOnInit() {
+    this.route.params.subscribe (
+      contact => {
+        let id = contact.id;
+        if (!id) {
+          this.editMode = false;
+          return;
+        } 
+        this.originalContact = this.contactService.getContact(id);
+        if (!this.originalContact) {
+          return;
+        } 
+        this.editMode = true;
+        this.contact = this.originalContact;
+  
+        if (this.contact.group) {
+          this.groupContacts = this.contact.group;
+        }
+    })
   }
 
   onCancel() {
@@ -26,6 +50,13 @@ export class ContactEditComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    console.log("wassap");
+    let value = form.value;
+    let newContact = new Contact(value.id, value.name, value.email, value.phone, value.imageUrl, value.group);
+    if (this.editMode == true) {
+      this.contactService.updateContact(this.originalContact, newContact);
+    } else {
+      this.contactService.addContact(newContact);
+    }
+    this.router.navigate(['../'], {relativeTo: this.route});
   }
 }
