847aaca7ae34eb3e07b1dbe7dde4d8ca0ca68b74
Alexandra Derby <alexandraderby@Alexandras-MacBook-Air.local>
2021-11-06 00:03:26 -0700

    Implementing the Behavior of the Document Edit Form  part 3 complete

--- cms/src/app/documents/document-edit/document-edit.component.ts
+++ cms/src/app/documents/document-edit/document-edit.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'cms-document-edit',
@@ -17,18 +19,46 @@ export class DocumentEditComponent implements OnInit {
   @ViewChild('f') documentForm: NgForm;
   subscription: Subscription;
 
+  constructor(
+    private documentService: DocumentService,
+    private router: Router,
+    private route: ActivatedRoute) {
 
-  constructor() { }
+}
 
-  ngOnInit(): void {
-  }
+ngOnInit() {
+  this.route.params
+  .subscribe (
+    (params: Params) => {
+      let id = this.document.id;
+        if (!id) {
+          this.editMode = false;
+          return;
+        }
+      this.originalDocument = this.documentService.getDocument(id);
+  
+      if (!this.originalDocument) {
+        return;
+      }
+      this.editMode = true;
+        this.document = JSON.parse(JSON.stringify(this.originalDocument));
+  }) 
+}
 
   onCancel() {
     console.log("test");
   }
 
   onSubmit(form: NgForm) {
-
+    let value = form.value;
+    let newDocument = new Document(value.id, value.name, value.description, value.url, value.children);
+    if (this.editMode == true) {
+      this.documentService.updateDocument(this.originalDocument, newDocument)
+    }
+    else {
+      this.documentService.addDocument(newDocument)
+    }
+    this.router.navigate(['../'], {relativeTo: this.route});
   }
 
 }
